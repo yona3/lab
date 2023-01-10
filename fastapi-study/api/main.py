96152597e517a909634edb69311ece8bc5c2638e
yona3 <kou.y.0905@icloud.com>
2023-01-10 10:16:53 +0900

    ✨  feat(pj:fastapi-study): APIルーターとスキーマの定義

--- fastapi-study/api/main.py
+++ fastapi-study/api/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
+from api.routers import done, task
+
 app = FastAPI()
-
-
-@app.get("/hello")
-async def hello() -> dict[str, str]:
-    return {"message": "hello FastAPI!"}
+app.include_router(task.router)
+app.include_router(done.router)
